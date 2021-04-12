1689a35a7bae29cb40ac2b22e5fde28a6d0c3369
Tom Trafford <tom.trafford@diamond.ac.uk>
2021-04-12 12:30:22 +0100

    Minor changes to get ioc to build

--- Makefile
+++ Makefile
@@ -5,5 +5,8 @@ include $(TOP)/configure/CONFIG
 DIRS := $(DIRS) $(filter-out $(DIRS), configure)
 DIRS := $(DIRS) $(filter-out $(DIRS), ADPluginKafkaApp)
 #DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard AD*))
-
+# ifeq ($(BUILD_IOCS), YES)
+# DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocs))
+# iocs_DEPEND_DIRS += ADPluginKafkaApp
+# endif
 include $(TOP)/configure/RULES_TOP
--- etc/builder.py
+++ etc/builder.py
@@ -2,21 +2,23 @@ from iocbuilder import Device, AutoSubstitution, Architecture, SetSimulation
 from iocbuilder.arginfo import *
 from iocbuilder.modules.asyn import Asyn, AsynPort, AsynIP
 
-from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, NDFileTemplate, makeTemplateInstance, includesTemplates
+from iocbuilder.modules.ADCore import ADCore, makeTemplateInstance, includesTemplates, NDPluginBaseTemplate
 
 # Library names differ between windows and linux.
 epics_host_arch = Architecture()
 is_windows = epics_host_arch.find('win') >= 0
 
-@includesTemplates(ADBaseTemplate, NDFileTemplate)
+@includesTemplates(NDPluginBaseTemplate)
 class kafkaPluginTemplate(AutoSubstitution):
     TemplateFile = "ADPluginKafka.template"
 
 class KafkaPlugin(AsynPort):
-    """This plugin can compress NDArrays to HDF5 and write them to file"""
-    # This tells xmlbuilder to use PORT instead of name as the row ID
     UniqueName = "PORT"
     _SpecificTemplate = kafkaPluginTemplate
+
+    DbdFileList = ['ADPluginKafka']
+    LibFileList = ['ADPluginKafka']
+    AutoInstantiate = True
 
     def __init__(self, PORT, NDARRAY_PORT, BROKER_ADDR, TOPIC, QUEUE = 3, BLOCK = 1, NDARRAY_ADDR = 0, **args):
         # Init the superclass (AsynPort)
@@ -34,6 +36,9 @@ class KafkaPlugin(AsynPort):
         NDARRAY_ADDR = Simple('Input array port address', int),
         BROKER_ADDR = Simple('Kafka Broker address', str),
         TOPIC = Simple('Kafka topic', str))
+
+
+
 
     def Initialise(self):
         print '# KafkaPluginConfigure(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr, maxMemory, brokerAddress, topic)' % self.__dict__
